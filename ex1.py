03518cc891758e2024fa22f2081327fb6475cdda
wonbin <ah0416@naver.com>
2024-10-01 21:26:04 +0900

    feature-branch1에서 수정

--- ex1.py
+++ ex1.py
@@ -3,14 +3,14 @@ num2 = float(input("두 번째 숫자: "))
 operation = input("+ or - or * or /: ")
 
 if operation == '+':
-    print(f"결과: {num1 + num2}")
+    print(f"결과는 {num1 + num2}")
 elif operation == '-':
-    print(f"결과: {num1 - num2}")
+    print(f"결과는 {num1 - num2}")
 elif operation == '*':
-    print(f"결과: {num1 * num2}")
+    print(f"결과는 {num1 * num2}")
 elif operation == '/':
     if num2 != 0:
-        print(f"결과: {num1 / num2}")
+        print(f"결과는 {num1 / num2}")
     else:
         print("0으로 나눌 수 없습니다.")
 else:
